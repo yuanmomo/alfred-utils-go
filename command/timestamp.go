75ef2ebd60ee5be15561a8efc5adfea4de76e2da
Hongbin.Yuan <harry.yuan@bybit.com>
2022-06-22 18:29:24 +0800

    add time convert

--- command/timestamp.go
+++ command/timestamp.go
@@ -2,6 +2,7 @@ package command
 
 // Package is called aw
 import (
+	"fmt"
 	aw "github.com/deanishe/awgo"
 	"strconv"
 	"strings"
@@ -60,6 +61,27 @@ func (c *TimestampCommand) Execute(wf *aw.Workflow, args []string) *aw.Workflow 
 		var t time.Time
 		var e error
 		if strings.Index(inputString, "T") > 0 {
+			// 转换 yyyy-MM-ddTHH:mm:ss 格式，做自动补齐
+			dateArray := strings.Split(inputString, "T")
+			if len(dateArray) == 1 {
+				// 输入 2022-06-21T 的格式
+				inputString = fmt.Sprintf("%sT00:00:00", dateArray[0])
+			} else {
+				// 输入 2022-06-21T 的格式
+				timeStringArray := strings.Split(dateArray[1], ":")
+				timeArray := [3]string{"00", "00", "00"}
+				for i := range timeStringArray {
+					if len(timeStringArray[i]) == 0 {
+						timeStringArray[i] = "0"
+					}
+
+					timeNumber, _ := strconv.Atoi(timeStringArray[i])
+					timeArray[i] = fmt.Sprintf("%02d", timeNumber)[0:2]
+				}
+
+				inputString = fmt.Sprintf("%sT%s:%s:%s", dateArray[0], timeArray[0], timeArray[1], timeArray[2])
+			}
+
 			// contains space
 			t, e = time.Parse(`2006-01-02T15:04:05`, inputString)
 		} else {
